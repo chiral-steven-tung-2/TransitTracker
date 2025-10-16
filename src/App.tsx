8bf4a90af0b9c5e0c0cba06409ca808e8b026c59
GreenMarioX <steventung666@gmail.com>
2025-10-16 00:23:25 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button variant="outline">Hello World</Button>
+      </header>
+    </div>
+  )
+}
+
+export default App
